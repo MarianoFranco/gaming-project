8495b41f43a84e6eaf1b172819c7e1d78057fae8
MarianoFranco <MarianoFranco@users.noreply.github.com>
2021-04-19 17:48:34 +0200

    adding js to the profile page

--- js/game_profile.js
+++ js/game_profile.js
@@ -1,2 +1,22 @@
 const queryString = document.location.search;
 console.log(queryString);
+
+const params = new URLSearchParams(queryString);
+console.log(params);
+
+const id = params.get('id');
+console.log(id);
+
+async function getId(gamesId){
+    try{
+        const response = await fetch('https://lvlupgames.argenteam.tech/wp-json/wc/store/products/' + gamesId);
+        const jsonResults = await response.json();
+		console.log(jsonResults);
+    }catch{
+
+    }finally{
+
+    }
+}
+
+getId(id);
